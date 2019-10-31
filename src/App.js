ce689bf529893bb77ca381d2b5c9f8ddf2b8cffc
Test <test@Tests-MacBook-Pro.local>
2019-10-31 14:21:37 -0400

    4.9

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents } from './api';
 import { OfflineAlert } from './Alert'
+import moment from 'moment';
+import {ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
 
 
 class App extends Component {
@@ -47,6 +49,28 @@ class App extends Component {
     //console.log(this.state.page)
     
   }
+
+  countEventsOnADate = (date) => {
+    let count = 0;
+    for (let i = 0; i < this.state.events.length; i += 1) {
+      if (this.state.events[i].local_date === date) {
+        count += 1;
+      }
+    }
+    return count;
+  }
+
+  getData = () => {
+    const next7Days = [];
+    const currentDate = moment();
+    for (let i = 0; i < 7; i += 1) {
+      currentDate.add(1, 'days');
+      const dateString = currentDate.format('YYYY-MM-DD');
+      const count = this.countEventsOnADate(dateString);
+      next7Days.push({ date: dateString, number: count });
+    }
+    return next7Days;
+  }
   
   render() {
     return (
@@ -55,6 +79,19 @@ class App extends Component {
           <OfflineAlert text={this.state.onlineStatus}/>
             <CitySearch updateEvents={this.updateEvents}/>
             <NumberOfEvents updateEvents={this.updateEvents}/>
+            <ResponsiveContainer height={400}>
+            <ScatterChart
+          margin={{
+            top: 20, right: 20, bottom: 20, left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="category" dataKey="date" name="date"/>
+          <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false}/>
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
+        </ResponsiveContainer>
         <EventList events={this.state.events}/>
       </div>
     );
