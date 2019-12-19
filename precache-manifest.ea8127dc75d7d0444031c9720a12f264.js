f5a00e1560687e0fd00c5263d2df2b392bca07a5
Test <test@Tests-MacBook-Pro.local>
2019-12-18 19:09:50 -0500

    Updates

--- precache-manifest.ea8127dc75d7d0444031c9720a12f264.js
+++ precache-manifest.ea8127dc75d7d0444031c9720a12f264.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7a2405b1b3beb25cc2a24ff40a013b61",
+    "revision": "a6712e20fdc055cf9dae0167160ddcea",
     "url": "/meetup/index.html"
   },
   {
-    "revision": "d6f18cec0314c5ae7baf",
+    "revision": "c8195b83d12139310b69",
     "url": "/meetup/static/css/main.d8498b0e.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/meetup/static/js/2.b4ed84aa.chunk.js"
   },
   {
-    "revision": "d6f18cec0314c5ae7baf",
-    "url": "/meetup/static/js/main.2a881d78.chunk.js"
+    "revision": "c8195b83d12139310b69",
+    "url": "/meetup/static/js/main.2c2ef8d3.chunk.js"
   },
   {
     "revision": "c48a5354a24f34a4db70",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.f6f7c2b6e578d717f7692eae91916f71.js"
+  "/meetup/precache-manifest.ea8127dc75d7d0444031c9720a12f264.js"
 );
 
 self.addEventListener('message', (event) => {
